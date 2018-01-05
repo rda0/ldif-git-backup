04f7374c907ef3193f13f62bb1e9898f60302463
Sven Mäder <maeder@phys.ethz.ch>
2018-01-05 19:00:00 +0100

    Read LDIF via stdin, subprocess or file

--- ldif-git-backup.py
+++ ldif-git-backup.py
@@ -8,26 +8,42 @@ import pathlib
 import git
 import collections
 import configparser
+import time
 
 def main(argv):
+    starttime = time.perf_counter()
+
     # Define default parameter values
     defaults = {
-        'ldif_cmd': '/usr/sbin/slapcat -n 1 -o ldif-wrap=no',
+        'ldif_cmd': '',
+        'ldif_file': '',
+        'ldif_stdin': False,
         'backup_dir': '/var/backups/ldap',
         'commit_msg': 'ldif-git-backup',
+        'exclude_attrs': '',
         'no_gc': False,
         'no_rm': False,
-        'exclude_attrs': '',
+        'ldif_wrap': False,
     }
 
     # Parse cmd-line arguments
     parser = argparse.ArgumentParser(add_help=False,
             description='Backup OpenLDAP databases using Git')
-    parser.add_argument('-c', '--ldif-cmd',
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('-c', '--ldif-cmd',
             dest='ldif_cmd',
             type=str,
             help='A command which returns an LDAP database in LDIF format '
             'including operational attributes or at least entryUUID')
+    group.add_argument('-l', '--ldif-file',
+            dest='ldif_file',
+            type=str,
+            help='Read LDIF from file')
+    group.add_argument('-i', '--stdin',
+            dest='stdin',
+            action='store_const',
+            const=True,
+            help='Read LDIF from stdin (default)')
     parser.add_argument('-d', '--backup-dir',
             dest='backup_dir',
             type=str,
@@ -36,6 +52,10 @@ def main(argv):
             dest='commit_msg',
             type=str,
             help='The commit message')
+    parser.add_argument('-e', '--exclude-attrs',
+            dest='exclude_attrs',
+            type=str,
+            help='Exclude all attributes matching the regular expression')
     parser.add_argument('--no-gc',
             dest='no_gc',
             action='store_const',
@@ -46,11 +66,12 @@ def main(argv):
             action='store_const',
             const=True,
             help='Do not perform a git rm')
-    parser.add_argument('-e', '--exclude-attrs',
-            dest='exclude_attrs',
-            type=str,
-            help='Exclude all attributes matching the regular expression')
-    parser.add_argument('--config-file',
+    parser.add_argument('-w', '--ldif-wrap',
+            dest='ldif_wrap',
+            action='store_const',
+            const=True,
+            help='LDIF input is wrapped')
+    parser.add_argument('-f', '--config-file',
             dest='config_file',
             default='ldif-git-backup.conf',
             type=str,
@@ -83,16 +104,24 @@ def main(argv):
     # Create param dict with chained default values
     param = collections.ChainMap(filtered_args, config, defaults)
 
-    # Define constants, compile regular expressions
-    ENTRY_SEP = '\n\n'
-    ATTR_WRAP = '\n '
+    # Define flags, compile regular expressions
+    ldif_from_cmd = False
+    ldif_from_file = False
+    ldif_wrapped = False
+    exclude_attrs = False
+    if not param['ldif_stdin']:
+        if not param['ldif_cmd']:
+            if param['ldif_file']:
+                ldif_from_file = True
+        elif param['ldif_cmd']:
+            ldif_from_cmd = True
+    if param['ldif_wrap']:
+        ldif_wrapped = True
     rgx_uuid = re.compile(r'\nentryUUID: ([^\n]+)')
     if param['exclude_attrs']:
         regex = r'(' + param['exclude_attrs'] + r'): '
         rgx_excl = re.compile(regex)
-        excl = True
-    else:
-        excl = False
+        exclude_attrs = True
 
     # Clean up ldif command
     ldif_cmd = re.sub('\s+', ' ', param['ldif_cmd']).split(' ')
@@ -104,55 +133,74 @@ def main(argv):
 
     # Initialize git repo, get file list from last commit
     repo = git.Repo.init(dpath)
+    new_commit_files = []
     if not param['no_rm']:
         if len(repo.heads) == 0:
             last_commit_files = []
         else:
             last_commit_files = [f.name for f in repo.head.commit.tree.blobs]
 
-    new_commit_files = []
+    # Determine LDIF input method
+    if ldif_from_file:
+        fin = open(param['ldif_file'], 'r')
+    elif ldif_from_cmd:
+        p = subprocess.Popen(ldif_cmd, stdout=subprocess.PIPE)
+        fin = p.stdout
+    else:
+        fin = sys.stdin
 
     # Stream from LDIF input and write LDIF files
-    with subprocess.Popen(ldif_cmd, stdout=subprocess.PIPE) as p:
-        entry = ''
-        attr = ''
-        uuid = None
-        while True:
-            output = p.stdout.readline()
-            if not output and p.poll() is not None:
-                break
-            line = output.decode('utf-8')
-            if line == '\n':
-                if not uuid:
-                    sys.exit('Error: no entryUUID attribute found!' +
-                        '\n\nEntry:\n\n' + entry)
-                entry = ''.join([entry, attr.rstrip()])
-                # Write LDIF file
-                fname = ''.join([uuid, '.ldif'])
-                fpath = ''.join([dpath, fname])
-                print(uuid)
-                with open(fpath, 'w') as fout:
-                    fout.write(''.join([entry, ENTRY_SEP]))
-                new_commit_files.append(fname)
-                # Prepare local variables for next entry
-                entry = ''
-                attr = ''
-                uuid = None
+    entry = ''
+    attr = ''
+    uuid = None
+    while True:
+        line = fin.readline()
+        # Exit the loop when finished reading
+        if not line:
+            break
+        # Optional decode bytes from subprocess
+        if ldif_from_cmd:
+            line = line.decode('utf-8')
+        # End of an entry
+        if line == '\n':
+            if not uuid:
+                sys.exit('Error: no entryUUID attribute found!' +
+                    '\n\nEntry:\n\n' + entry)
+            if ldif_wrapped:
+                entry = ''.join([entry, attr])
+            # Write LDIF file
+            fname = ''.join([uuid, '.ldif'])
+            fpath = ''.join([dpath, fname])
+            with open(fpath, 'w') as fout:
+                fout.write(''.join([entry, '\n']))
+            new_commit_files.append(fname)
+            # Prepare local variables for next entry
+            entry = ''
+            attr = ''
+            uuid = None
+            continue
+        # Get the entryUUID
+        elif line.startswith('entryUUID: '):
+            uuid = line.split('entryUUID: ', 1)[1].rstrip()
+        # Filter out attributes
+        if exclude_attrs:
+            match_excl = rgx_excl.match(line)
+            if match_excl:
                 continue
-            elif line.startswith('entryUUID: '):
-                uuid = line.split('entryUUID: ', 1)[1].rstrip()
-            if excl:
-                match_excl = rgx_excl.match(line)
-                if match_excl:
-                    continue
+        # Append the lines to theentry
+        if ldif_wrapped:
             if line.startswith(' '):
                 attr = ''.join([attr.rstrip(), line[1:]])
             else:
                 entry = ''.join([entry, attr])
                 attr = ''
                 attr = ''.join([attr, line])
-        rc = p.poll()
-        #print('exit_code:', str(rc))
+        else:
+            entry = ''.join([entry, line])
+
+    # Close file
+    if ldif_from_file:
+        fin.close()
 
     # Add new LDIF files to index (stage)
     repo.index.add(new_commit_files)
@@ -169,6 +217,11 @@ def main(argv):
     # Clean up the repo
     if not param['no_gc']:
         repo.git.gc('--auto')
+
+    # Measure execution time
+    endtime = time.perf_counter()
+    runtime = endtime - starttime
+    print('runtime:', str(runtime), 's')
 
 if __name__== "__main__":
     main(sys.argv)
